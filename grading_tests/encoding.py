27acb79bf41ae82ed0eb0fcbe51dcf9a1e2e3b59
Oleg Fafurin <olegfafurin1@gmail.com>
2023-10-29 16:11:16 +0100

    uncorrelation

--- grading_tests/encoding.py
+++ grading_tests/encoding.py
@@ -1,12 +1,11 @@
 import numpy as np
 from helpers import *
-from collections import defaultdict
 
 
 def transform_train(feature):
     m = dict()
     for x in feature:
-        if x not in m:
+        if x not in m and not np.isnan(x):
             m[x] = len(m)
     f = np.vstack((np.eye(len(m)), np.zeros(len(m))))
     u = f[np.vectorize(lambda key: m.get(key, len(m)))(feature)]
@@ -18,9 +17,6 @@ def transform_test(feature, m):
     f = np.vstack((np.eye(n_uniq), np.zeros(n_uniq)))
     ind = np.array([m[k] if k in m else n_uniq for k in feature])
     return f[ind]
-
-data_path = '../data/dataset/dataset_to_release'
-x_train_preclean, x_test_preclean, y_train, train_ids, test_ids = load_csv_data(data_path)
 
 
 def percentageFilled(data):
@@ -35,9 +31,6 @@ def threshold_col_filter(data, threshold):
 def non_constant_filter(data):
     return np.logical_not(np.logical_or(np.isnan(np.nanstd(data, 0)), np.nanstd(data, 0) == 0))
 
-
-cat_threshold = 10
-
 def standardize(x):
     """Standardize the original data set."""
     std = np.nanstd(x, axis=0)
@@ -45,58 +38,91 @@ def standardize(x):
     return np.nan_to_num((x - np.nanmean(x, axis=0)) / np.nanstd(x, axis=0)), mean, std
 
 
-def process_train(data):
+def process_train(data, corr_threshold = 0.5, cat_threshold = 10, verbose = False):
     n, m = data.shape
     filter = np.logical_and(threshold_col_filter(data, 0.2), non_constant_filter(data))
     categorical_filter = np.apply_along_axis(lambda x: len(set(x)) < cat_threshold, 0, data)
     cat_transform = dict()
     num_transform = dict()
-    res = np.empty((n, 0))
+    features = np.empty((n, 0))
+    transform_log = list()
+    cnt = 0
     for i in range(m):
         if not filter[i]:
+            transform_log.append(f"{i} deleted")
+            cnt += 1
             continue
         if categorical_filter[i]:
             encoded, mp = transform_train(data[:, i])
+            l = encoded.shape[1]
+            transform_log.append(f"{i} cat exp: {cnt}-{cnt + l - 1}")
+            cnt += l
             cat_transform[i] = mp
-            res = np.append(res, encoded, axis=1)
+            features = np.append(features, encoded, axis=1)
         else:
+            transform_log.append(f"{i} num: {cnt}")
+            cnt += 1
             x_num_std, mean, std = standardize(data[:, i])
             x_num_std[abs(x_num_std) > 3] = 0
             num_transform[i] = (mean, std)
-            res = np.append(res, x_num_std.reshape((n,1)), axis=1)
-    return res, filter, categorical_filter, num_transform, cat_transform
+            features = np.append(features, x_num_std.reshape((n,1)), axis=1)
+    corr_filter = np.full((features.shape[1],), True, dtype=bool)
+    cm = np.corrcoef(features, rowvar=False)
+    for i in range(len(cm)):
+        if corr_filter[i]:
+            for j in range(i + 1, len(cm)):
+                if corr_filter[j] and abs(cm[i][j]) >= corr_threshold:
+                    corr_filter[j] = False
+    if verbose:
+        print(f"number of features with pairwise corr <= {corr_threshold}: {sum(corr_filter)}")
+
+    if verbose:
+        print("\n".join(transform_log))
+    return features[:, corr_filter], filter, categorical_filter, corr_filter, num_transform, cat_transform
 
 
-def process_test(data, filter, categorical_filter, num_transform, cat_transform):
+def process_test(data, filter, categorical_filter, corr_filter, num_transform, cat_transform, verbose=False):
     n, m = data.shape
     res = np.empty((n, 0))
+    transform_log = list()
+    cnt = 0
     for i in range(m):
         if not filter[i]:
+            transform_log.append(f"{i} deleted")
+            cnt += 1
             continue
         if categorical_filter[i]:
+            l = len(cat_transform[i])
+            transform_log.append(f"{i} cat exp: {cnt}-{cnt + l - 1}")
+            cnt += l
             res = np.append(res, transform_test(data[:, i], cat_transform[i]), axis=1)
         else:
+            transform_log.append(f"{i} num: {cnt}")
+            cnt += 1
             mean, std = num_transform[i] # std shouldn't be 0
             res = np.append(res, np.nan_to_num((data[:, i] - mean) / std).reshape((n,1)), axis=1)
-    return res
+    if verbose:
+        print("\n".join(transform_log))
+    return res[:, corr_filter]
 
 
-x_train, filter, categorical_filter, num_transform, cat_transform = process_train(x_train_preclean)
+if __name__ == '__main__':
+    data_path = '../data/dataset/debug'
+    x_train_preclean, x_test_preclean, y_train, train_ids, test_ids = load_csv_data(data_path)
 
+    x_train, filter, categorical_filter, corr_filter, num_transform, cat_transform = process_train(x_train_preclean, verbose=True)
+    x_test = process_test(x_test_preclean, filter, categorical_filter, corr_filter, num_transform, cat_transform, verbose=True)
 
-x_test = process_test(x_test_preclean, filter, categorical_filter, num_transform, cat_transform)
+    np.set_printoptions(precision=2)
 
+    print(x_train.shape)
+    print(x_train)
 
-np.set_printoptions(precision=2)
+    print("-------------")
+    print("-------------")
+    print("-------------")
+    print("-------------")
+    print("-------------")
 
-print(x_train.shape)
-print(x_train)
-
-print("-------------")
-print("-------------")
-print("-------------")
-print("-------------")
-print("-------------")
-
-print(x_test.shape)
-print(x_test)
+    print(x_test.shape)
+    print(x_test)
